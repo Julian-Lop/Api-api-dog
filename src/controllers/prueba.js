b0a8921377db345d58ecb93e0b99306518c4c6a5
Julian-Lop <you@example.com>
2022-03-01 12:30:19 -0500

    new branch subtrree

--- src/controllers/prueba.js
+++ src/controllers/prueba.js
@@ -2,5 +2,5 @@ const { Router} = require('express');
 
 
 exports.prueba = (req,res) => {
-    return res.status(200).send('Esto es una prueba con rama')
+    return res.status(200).send('Esto es una prueba con otra branch, probando si se hace merge desde el subtree')
 }

21f552edaef7fc72088200558c95fef83f4bad95
Julian-Lop <you@example.com>
2022-03-01 12:24:54 -0500

    new branch

--- src/controllers/prueba.js
+++ src/controllers/prueba.js
@@ -2,5 +2,5 @@ const { Router} = require('express');
 
 
 exports.prueba = (req,res) => {
-    return res.status(200).send('esto es una prueba')
+    return res.status(200).send('Esto es una prueba con rama')
 }
